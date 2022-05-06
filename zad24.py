edaed3b39b9c299eebab4a8b46849f45a4024015
svset <100479022+svset@users.noreply.github.com>
2022-05-07 01:38:38 +0200

    Add files via upload

---
+++ zad24.py
@@ -0,0 +1,11 @@
+print("Drukujemy liczby z zakresu:")
+for i in range(1, 20, 3):
+    print(i)
+
+print("Drukujemy liczby z listy:")
+for i in [4, 5, 10, 2, 5]:
+    print(i)
+
+print("Drukujemy wielokrotnie napis:")
+for i in range(5):
+    print("Hi!")
