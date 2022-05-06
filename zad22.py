4e35be116edcd0c32b4f0bd9cdd5facfef8e2170
svset <100479022+svset@users.noreply.github.com>
2022-05-07 01:31:40 +0200

    Add files via upload

---
+++ zad22.py
@@ -0,0 +1,11 @@
+lnpp5 = [5, 10, 15, 20, 25]
+lnpp5.append(30)
+print(len(lnpp5))
+print(lnpp5[1:3])
+print(lnpp5[0:4])
+
+print(lnpp5[::-1])
+(lnpp5.pop(3))
+print(lnpp5)
+print(max(lnpp5))
+print(sum(lnpp5))
